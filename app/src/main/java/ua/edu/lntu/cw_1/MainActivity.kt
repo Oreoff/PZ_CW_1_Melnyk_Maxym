b8456ee6af15504e6b49c99d950d3dbd7f299e69
Oreoff <maximmelnik37@gmail.com>
2024-03-11 14:19:33 +0200

    added image into program

--- app/src/main/java/ua/edu/lntu/cw_1/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_1/MainActivity.kt
@@ -1,17 +1,21 @@
 package ua.edu.lntu.cw_1
-
+import androidx.compose.foundation.layout.*
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw_1.ui.theme.CW_1Theme
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +35,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting( modifier: Modifier = Modifier) {
-
+Column(modifier = Modifier.padding(13.dp).fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally
+)
+    {
+Column(modifier = modifier.padding(13.dp),
+    verticalArrangement = Arrangement.Center)
+{
+    Image(painter = painterResource(id = R.drawable.bg_f8f8f8_flat_750x_075_f_pad_750x1000_f8f8f8),
+        contentDescription = "Photo of author")
+}
+    }
 }
 
 @Preview(showBackground = true)
